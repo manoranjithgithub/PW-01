da3f712d9a4f17fb8dcfd43f2f096f1a083b8d8c
priyamaran <priyamaran538@gmail.com>
2025-10-03 19:29:10 +0530

    bugfixes

--- src/app/pages/deployments/deployment-settings/deployment-settings.component.ts
+++ src/app/pages/deployments/deployment-settings/deployment-settings.component.ts
@@ -228,8 +228,8 @@ export class DeploymentSettingsComponent implements OnInit, AfterViewInit {
         // this.showCustomDnsHost = !!res.data.is_custom_dns;
         //General settings
         const rawGitUrl = res.data.sourceCode?.gitUrl || '';
-        const [urlPart, , branch] = rawGitUrl.split(' ') || [];
-        const cleanUrl = urlPart.replace(/\/\/.*@/, '//').replace(/\.git$/, '') || '';
+        const [urlPart, , branch] = rawGitUrl?.split(' ') || [];
+        const cleanUrl = urlPart?.replace(/\/\/.*@/, '//').replace(/\.git$/, '') || '';
         const provider = cleanUrl?.split('/')[2]?.split('.')[0] || '';
         const repoUrl = cleanUrl;
         const branchName = branch || '';
