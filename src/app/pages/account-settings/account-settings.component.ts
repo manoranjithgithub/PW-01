17a2f15c396563da797b4c1918d0de8f36d763f6
priyamaran <priyamaran538@gmail.com>
2025-10-03 13:11:52 +0530

    vcs fixes

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -41,7 +41,7 @@ export class AccountComponent implements OnInit {
 
     this.resetPasswordForm = this.fb.group({
       oldPassword: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&=+/-]{8,}$/)]],
       confirmPassword: ['', Validators.required],
     }, { validators: this.passwordsMatchValidator });
 
